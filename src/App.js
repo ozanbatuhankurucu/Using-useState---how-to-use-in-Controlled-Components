3dce0070667a72508b4fd19d98cce35ccdf0231e
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2021-12-25 16:26:10 +0300

    Complete using useState() - Controlled Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
 function App() {
+  const [userInfo, setUserInfo] = useState({
+    name: '',
+    password: '',
+    email: ''
+  })
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true)
+
+  const handleOnChange = (event) => {
+    const {
+      target: { value, name }
+    } = event
+
+    setUserInfo((prev) => {
+      return {
+        ...prev,
+        [name]: value
+      }
+    })
+  }
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault()
+    alert('Successfuly registered!')
+  }
+
+  useEffect(() => {
+    const { name, password, email } = userInfo
+    const isButtonEnabled = !!name && !!password && !!email
+    setIsButtonDisabled(!isButtonEnabled)
+  }, [userInfo])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form onSubmit={handleOnSubmit}>
+        <label htmlFor='name'>Name</label>
+        <input value={userInfo.name} id='name' name='name' type='text' onChange={handleOnChange} />
+        <label htmlFor='email'>Email</label>
+        <input value={userInfo.email} name='email' type='email' onChange={handleOnChange} />
+        <label htmlFor='password'>Password</label>
+        <input
+          value={userInfo.password}
+          id='password'
+          name='password'
+          type='password'
+          onChange={handleOnChange}
+        />
+        <button disabled={isButtonDisabled} type='submit'>
+          Submit
+        </button>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
